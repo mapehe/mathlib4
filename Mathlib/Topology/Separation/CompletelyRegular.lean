392ab64b345f0dd816ca468c7e7fb81e221d64f2
Matias Heikkilä <matias@three.consulting>
2025-06-14 10:41:33 +0300

    asd¨

--- Mathlib/Topology/Separation/CompletelyRegular.lean
+++ Mathlib/Topology/Separation/CompletelyRegular.lean
@@ -205,7 +205,7 @@ lemma exists_continuous_image_of_stoneCech
     have dns : Dense (range (stoneCechExtend C)) := sorry
     have amp : closure (range (stoneCechExtend C)) = range (stoneCechExtend C) := sorry
     rw [←amp]
-    sorry
+    apply dns.closure_eq
   exact ⟨S, stoneCechExtend_extends C⟩
 
 
