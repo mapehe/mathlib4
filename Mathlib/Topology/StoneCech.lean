12905014d2c0bcb7c37723d548bdfca3c02ba83c
Matias Heikkilä <matias@three.consulting>
2025-06-14 11:45:44 +0300

    asd¨

--- Mathlib/Topology/StoneCech.lean
+++ Mathlib/Topology/StoneCech.lean
@@ -402,7 +402,7 @@ lemma exists_continuous_image_of_stoneCech
     have dns : Dense (range (stoneCechExtend C)) := by
       rw [←DenseRange]
       rw [←stoneCechExtend_extends C] at d
-      sorry
+      exact DenseRange.comp_left d
     have amp : closure (range (stoneCechExtend C)) = range (stoneCechExtend C) := by
       rw [IsClosed.closure_eq]
       apply IsCompact.isClosed
